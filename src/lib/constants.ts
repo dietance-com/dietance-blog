af78def9e910b6949e216ec7edf57afa7d67e0da
Yudhvir Singh Godara <cyberallianceindia@gmail.com>
2025-08-28 01:26:19 +0530

    updated logo

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,4 +1,4 @@
 export const EXAMPLE_PATH = "blog-starter";
 export const CMS_NAME = "Dietance";
 export const HOME_OG_IMAGE_URL =
-  "/public/assets/blog/images/logo-w-tagline.png";
+  "https://www.dietance.com/assets/img/branding/logo-w-tagline.webp";
